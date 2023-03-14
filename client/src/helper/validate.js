778f3bf1edf3dcf3557dd7c5abd24e7db6a4e28d
Tu Nguyen <nguyenminhtuk52@gmail.com>
2023-03-14 22:53:24 +0700

    ui and validate password reset password

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -1,10 +1,40 @@
 import { toast } from "react-hot-toast";
-//
+// pass
+function passwordVerify(errors = {}, values){
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    if(!values.password){
+        errors.password = toast.error("Password Required...!");
+    } else if(values.password.includes(" ")){
+        errors.password = toast.error("Wrong Password...!");
+    }else if(values.password.length < 4){
+        errors.password = toast.error("Password must be more than 4 characters long");
+    }else if(!specialChars.test(values.password)){
+        errors.password = toast.error("Password must have special character");
+    }
+
+    return errors;
+}
+//pass
+export async function passwordValidate(values){
+    const errors = passwordVerify({}, values);
+    return errors;
+}
+//reset pass
+export async function resetPasswordValidation(values){
+    const errors = passwordVerify({}, values);
+
+    if(values.password !== values.confirm_pwd){
+        errors.exist = toast.error("Password not match...!");
+    }
+
+    return errors;
+}
+//user
 export async function usernameValidate(values) {
     const errors = usernameVerify({}, values);
     return errors;
 }
-//
+//user
 function usernameVerify(error = {}, values) {
     if (!values.username) {
         error.username = toast.error('Username Required');
