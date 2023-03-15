53ed0ce217c4ddf67e84bb64a35262f3a7f1d493
Tu Nguyen <nguyenminhtuk52@gmail.com>
2023-03-15 21:01:15 +0700

    update validate email ui register and convert iamge to base64

---
+++ client/src/helper/convert.js
@@ -0,0 +1,13 @@
+//chuyen image sang base64
+export default function converttoBase64(file) {
+    return new Promise((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+        fileReader.onload = () => {
+            resolve(fileReader.result);
+        }
+        fileReader.onerror = (error) => {
+            reject(error);
+        }
+    })
+}
--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -38,8 +38,28 @@ export async function usernameValidate(values) {
 function usernameVerify(error = {}, values) {
     if (!values.username) {
         error.username = toast.error('Username Required');
-    } else if (values.username.includes("")) {
+    } else if (values.username.includes(" ")) {
         error.username = toast.error('Invalid Username');
     }
+    return error;
+}
+//register
+export async function registerValidation(values){
+    const errors = usernameVerify({}, values);
+    passwordVerify(errors, values);
+    emailVerify(errors, values);
+
+    return errors;
+}
+//email
+function emailVerify(error ={}, values){
+    if(!values.email){
+        error.email = toast.error("Email Required...!");
+    }else if(values.email.includes(" ")){
+        error.email = toast.error("Wrong Email...!")
+    }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+        error.email = toast.error("Invalid email address...!")
+    }
+
     return error;
 }
