b44eda0dcf21c664be445d686c9cd4e29449b2fe
Tu Nguyen <nguyenminhtuk52@gmail.com>
2023-03-17 23:40:28 +0700

    update register backend

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,11 +1,17 @@
 import mongoose from "mongoose";
+
 import { MongoMemoryServer } from "mongodb-memory-server";
-async function connect() {
+
+async function connect(){
+
     const mongod = await MongoMemoryServer.create();
     const getUri = mongod.getUri();
+
     mongoose.set('strictQuery', true)
     const db = await mongoose.connect(getUri);
-    console.log("Database Connected");
+    // const db = await mongoose.connect(ENV.ATLAS_URI);
+    console.log("Database Connected")
     return db;
-};
+}
+
 export default connect;
