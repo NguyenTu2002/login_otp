3dcd95501c450bc280c508d427d7d5ad96e0f36c
Tu Nguyen <nguyenminhtuk52@gmail.com>
2023-03-18 15:56:09 +0700

    update login get and update user

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -1,15 +1,32 @@
 import UserModel from '../model/User.model.js'
 import bcrypt from 'bcrypt';
-export async function register(req,res){
+import jwt from 'jsonwebtoken'
+import ENV from '../config.js'
+//
+export async function verifyUser(req, res, next) {
+    try {
+        const { username } = req.method == "GET" ? req.query : req.body;
+
+        // check the user existance
+        let exist = await UserModel.findOne({ username });
+        if (!exist) return res.status(404).send({ error: "Can't find User!" });
+        next();
+
+    } catch (error) {
+        return res.status(404).send({ error: "Authentication Error" });
+    }
+}
+//
+export async function register(req, res) {
 
     try {
-        const { username, password, profile, email } = req.body;        
+        const { username, password, profile, email } = req.body;
 
         // check the existing user
         const existUsername = new Promise((resolve, reject) => {
-            UserModel.findOne({ username }, function(err, user){
-                if(err) reject(new Error(err))
-                if(user) reject({ error : "Please use unique username"});
+            UserModel.findOne({ username }, function (err, user) {
+                if (err) reject(new Error(err))
+                if (user) reject({ error: "Please use unique username" });
 
                 resolve();
             })
@@ -17,9 +34,9 @@ export async function register(req,res){
 
         // check for existing email
         const existEmail = new Promise((resolve, reject) => {
-            UserModel.findOne({ email }, function(err, email){
-                if(err) reject(new Error(err))
-                if(email) reject({ error : "Please use unique Email"});
+            UserModel.findOne({ email }, function (err, email) {
+                if (err) reject(new Error(err))
+                if (email) reject({ error: "Please use unique Email" });
 
                 resolve();
             })
@@ -28,10 +45,10 @@ export async function register(req,res){
 
         Promise.all([existUsername, existEmail])
             .then(() => {
-                if(password){
+                if (password) {
                     bcrypt.hash(password, 10)
-                        .then( hashedPassword => {
-                            
+                        .then(hashedPassword => {
+
                             const user = new UserModel({
                                 username,
                                 password: hashedPassword,
@@ -41,12 +58,12 @@ export async function register(req,res){
 
                             // return save result as a response
                             user.save()
-                                .then(result => res.status(201).send({ msg: "User Register Successfully"}))
-                                .catch(error => res.status(500).send({error}))
+                                .then(result => res.status(201).send({ msg: "User Register Successfully" }))
+                                .catch(error => res.status(500).send({ error }))
 
                         }).catch(error => {
                             return res.status(500).send({
-                                error : "Enable to hashed password"
+                                error: "Enable to hashed password"
                             })
                         })
                 }
@@ -60,15 +77,83 @@ export async function register(req,res){
     }
 
 }
+//
 export async function login(req, res) {
-    res.json('login');
-};
+
+    const { username, password } = req.body;
+
+    try {
+
+        UserModel.findOne({ username })
+            .then(user => {
+                bcrypt.compare(password, user.password)
+                    .then(passwordCheck => {
+
+                        if (!passwordCheck) return res.status(400).send({ error: "Don't have Password" });
+
+                        // create jwt token
+                        const token = jwt.sign({
+                            userId: user._id,
+                            username: user.username
+                        }, ENV.JWT_SECRET, { expiresIn: "24h" });
+                        return res.status(200).send({
+                            msg: "Login Successful...!",
+                            username: user.username,
+                            token
+                        });
+
+                    })
+                    .catch(error => {
+                        return res.status(400).send({ error: "Password does not Match" })
+                    })
+            })
+            .catch(error => {
+                return res.status(404).send({ error: "Username not Found" });
+            })
+
+    } catch (error) {
+        return res.status(500).send({ error });
+    }
+}
+
 export async function getUser(req, res) {
-    res.json('get user');
-};
-export async function updateUser(req, res) {
-    res.json('updateUser');
-};
+    const { username } = req.params;
+    try {
+        if (!username) return res.status(501).send({ error: "Invalid Username" });
+        UserModel.findOne({ username }, function (err, user) {
+            if (err) return res.status(500).send({ err });
+            if (!user) return res.status(501).send({ error: "Couldn't Find the User" });
+            const { password, ...rest } = Object.assign({}, user.toJSON());
+            return res.status(201).send(rest);
+        })
+    } catch (error) {
+        return res.status(404).send({ error: "Cannot Find User Data" });
+    }
+}
+export async function updateUser(req,res){
+    try {
+        
+        const id = req.query.id;
+        // const { userId } = req.user;
+
+        if(id){
+            const body = req.body;
+
+            // update the data
+            UserModel.updateOne({ _id : id }, body, function(err, data){
+                if(err) throw err;
+
+                return res.status(201).send({ msg : "Record Updated...!"});
+            })
+
+        }else{
+            return res.status(401).send({ error : "User Not Found...!"});
+        }
+
+    } catch (error) {
+        return res.status(401).send({ error });
+    }
+}
 export async function generateOTP(req, res) {
     res.json('generateOTP');
 };
